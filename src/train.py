7474e8b7c57c604948fc0311663a401086e2c136
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-10 11:47:29 +0100

    Implemented training with gridsearch

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import GridSearchCV
 from src.data_loader import load_dataset
 from src.config import (
     DATA_PATH,
@@ -28,15 +28,36 @@ def train():
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
-    model = RandomForestClassifier(
-        n_estimators=300,
+    param_grid = {
+        "n_estimators": [200, 300],
+        "max_depth": [None, 10, 20],
+        "min_samples_split": [2, 5],
+        "min_samples_leaf": [1, 2],
+    }
+
+    rf = RandomForestClassifier(
         random_state=RANDOM_STATE,
         class_weight="balanced",
+        n_jobs=-1,
     )
 
-    model.fit(X_train_scaled, y_train)
+    grid = GridSearchCV(
+        rf,
+        param_grid,
+        cv=3,
+        scoring="f1_weighted",
+        n_jobs=-1,
+        verbose=1,
+    )
 
-    joblib.dump(model, MODEL_PATH)
+    grid.fit(X_train_scaled, y_train)
+
+    best_model = grid.best_estimator_
+
+    joblib.dump(best_model, MODEL_PATH)
     joblib.dump(scaler, SCALER_PATH)
 
-    return model, X_test_scaled, y_test
+    print("Best params:", grid.best_params_)
+
+    return best_model, X_test_scaled, y_test
+
